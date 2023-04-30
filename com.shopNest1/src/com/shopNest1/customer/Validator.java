ec7f47f86862d36e860bb408124fa29c4ff6baff
vishwaraj <vishwarajrm9@gmail.com>
2023-05-01 00:31:41 +0530

    First Commit

---
+++ com.shopNest1/src/com/shopNest1/customer/Validator.java
@@ -0,0 +1,18 @@
+package com.shopNest1.customer;
+
+
+
+import com.shopNest1.dbHandler.DataFetcher;
+
+public class Validator {
+
+	public static boolean isValid(String uname, String pass) {
+			String dbPass=DataFetcher.fetchPassword(uname);
+			if(pass.equals(dbPass)) {
+				return true;
+			}
+			else {
+				return false;
+			}
+	}
+}
